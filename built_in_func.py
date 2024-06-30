53304ecc9d10760c3163657cc85d516f0870b580
oguzhanaras <oguzhanarasx@gmail.com>
2024-06-30 19:13:28 +0300

    Update built_in_func.py

--- built_in_func.py
+++ built_in_func.py
@@ -10,3 +10,17 @@ denominator = input("payda gir: ")
 result = int(numerator) / int(denominator)
 
 print(result, type(result))
+
+
+# unicode
+prompt = "unicode değerini istediğiniz karakter giriniz:"
+character = input(prompt)
+unicode_value = ord(character)
+print(character, unicode_value, type(unicode_value))
+
+
+# chr
+prompt = "unicode değerini istediğiniz karakter giriniz:"
+character = int(input(prompt))
+unicode_value = chr(character)
+print(f"unicode value: {unicode_value} - character: {character}")
