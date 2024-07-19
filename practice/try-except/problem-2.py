bc1411ebe8c9b8156677dae8a62bd3ed938ebba8
oguzhanaras <oguzhanarasx@gmail.com>
2024-07-19 22:35:18 +0300

    Update problem-2.py
    optimize

--- practice/try-except/problem-2.py
+++ practice/try-except/problem-2.py
@@ -1,21 +1,11 @@
 my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 def cift_mi(sayi):
+    if sayi % 2 == 0:
+        return sayi
+    else:
+        raise ValueError
+for i in my_list:
     try:
-        if sayi % 2 == 0:
-            return print(sayi)
-        else:
-            raise ValueError
-    except:
-        print("value error")
-    finally:
-        print("program bitti")
-
-cift_mi(5)
-cift_mi(2)
-
-try:
-    for i in my_list:
-        if i % 2 == 0:
-            print(i)
-except:
-    print("error")
+        print(cift_mi(i))
+    except ValueError:
+        pass
